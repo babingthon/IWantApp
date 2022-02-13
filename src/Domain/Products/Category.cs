2ed7080a23b231b20358753b885a65d87ff16607
babingthon <babingthon.bandeira@gmail.com>
2022-02-13 11:53:16 -0300

    first commit

---
+++ src/Domain/Products/Category.cs
@@ -0,0 +1,39 @@
+﻿using Flunt.Validations;
+
+namespace IWantApp.Domain.Products;
+
+public class Category : Entity
+{
+    public string Name { get; set; }
+    public bool Active { get; set; }
+
+    public Category(string name, string createBy, string editedBy)
+    {
+        Name = name;
+        Active = true;
+        CreateBy = createBy;
+        CreateOn = DateTime.Now;
+        EditedBy = editedBy;
+        EditedOn = DateTime.Now;
+
+        Validate();
+
+    }
+
+    private void Validate()
+    {
+        var contract = new Contract<Category>()
+                    .IsNotNullOrEmpty(Name, "Name")
+                    .IsGreaterOrEqualsThan(Name, 3, "Name")
+                    .IsNotNullOrEmpty(CreateBy, "CreateBy")
+                    .IsNotNullOrEmpty(EditedBy, "EditedOn");
+        AddNotifications(contract);
+    }
+    public void EditInfo(string name, bool active)
+    {
+        Name = name;
+        Active = active;
+
+        Validate();
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,25 @@
+using IWantApp.Endpoints.Categories;
+using IWantApp.Infra.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration["ConnectionStrings:IWantDb"]);
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapMethods(CategoryPost.Template, CategoryPost.Methods, CategoryPost.Handle);
+app.MapMethods(CategoryGetAll.Template, CategoryGetAll.Methods, CategoryGetAll.Handle);
+app.MapMethods(CategoryPut.Template, CategoryPut.Methods, CategoryPut.Handle);
+
+app.Run();
